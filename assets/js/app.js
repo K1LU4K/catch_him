bcec602bb66ce6848f179f03c8cdfde75d7fe64f
Gautier <gautier.dujarrier@gmail.com>
2018-03-17 12:59:43 +0100

    Version 1.7

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,8 +12,9 @@
         timeActive = grid.dataset.timeActive,
         timeBetweenChange = parseInt(timeActive) + 1000;
 
-    // Variables for winnable cell
+    // Variables for cell and win cell
     let cell,
+        winCell,
         cellId,
         changeCell,
         makeCellDisappear,
@@ -21,12 +22,19 @@
 
     let xhr = new XMLHttpRequest();
 
+    start.addEventListener("click", startGame, { once: true });
+
     /**
-     * Start the game
+     * Function for the click event in play buton, start the game
      * @param event
      */
-    let startGame = function (event) {
+    function startGame(event) {
         event.preventDefault();
+
+        winCell = document.querySelector(".win");
+        if (winCell) {
+            winCell.classList.remove("win");
+        }
 
         this.innerText = "Stop ?";
 
@@ -40,38 +48,12 @@
          * @type {number}
          */
         changeCell = setInterval(function () {
-            xhr.open("GET", "?difficulty=" + difficulty + "&grid-size=" + gridSize + "&ajax=true&cell=change");
 
-            xhr.onload = function() {
-
-                if (xhr.status === 200) {
-
-                    cellId = "cell" + xhr.responseText;
-                    cell = document.getElementById(cellId);
-                    cell.classList.add("active");
-
-                    // Timeout to disable winnable cell
-                    makeCellDisappear = setTimeout(function () {
-                        cell.classList.remove('active');
-                        clearTimeout(makeCellDisappear);
-                    }, timeActive);
-
-                    // Add click event to make the cell winnable
-                    cell.addEventListener("mousedown", makeWinnable);
-                    TimeoutCellWinnable = setTimeout(function () {
-                        cell.removeEventListener("mousedown", makeWinnable);
-                    }, timeActive);
-
-                }
-                else {
-                    alert('Une erreur est survenu, sorry.');
-                    clearInterval(changeCell);
-                }
-
-            };
-
+            xhr.open("GET", "?difficulty=" + encodeURIComponent(difficulty) + "&grid-size=" + encodeURIComponent(gridSize) + "&ajax=true&cell=change");
+            xhr.onload = setRandomCell;
             xhr.send();
-        }, timeBetweenChange);
+
+            }, timeBetweenChange);
 
     }
 
@@ -79,45 +61,91 @@
      * Function for the click event in the cell winnable
      * @param event
      */
-    let makeWinnable = function (event) {
+    function makeWinnable(event) {
         event.preventDefault();
 
-        let thisCell = this,
-            clickedCellId = thisCell.id;
+        var clickedCellId = cell.id;
 
-        xhr.open("GET", "?ajax=true&cell=isWinner&cellId=" + clickedCellId);
+        xhr.open("GET", "?ajax=true&cell=isWinner&cellId=" + encodeURIComponent(clickedCellId));
 
-        xhr.onload = function () {
-
-            if (xhr.status === 200) {
-
-                start.innerText = "Play ?";
-
-                start.addEventListener("click", startGame, { once:true });
-
-                alert(xhr.responseText);
-
-                clearInterval(changeCell);
-
-                clearTimeout(makeCellDisappear);
-                clearTimeout(TimeoutCellWinnable);
-
-                thisCell.classList.remove("active");
-                thisCell.removeEventListener("click", makeWinnable);
-
-            }
-            else {
-                alert('Une erreur est survenu, sorry.');
-                clearInterval(changeCell);
-                clearTimeout(makeCellDisappear);
-            }
-
-        }
+        xhr.onload = checkIfWin;
 
         xhr.send();
 
     }
 
-    start.addEventListener("click", startGame, { once: true });
+    /**
+     * Function for XMLHttpRequest.onload method, get a random cell form the database and active it during x millisecondes
+     */
+    function setRandomCell() {
+
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+
+            if (xhr.status === 200) {
+
+                cellId = "cell" + xhr.responseText;
+                cell = document.getElementById(cellId);
+
+                cell.classList.add("active");
+
+                // Timeout to disable winnable cell
+                makeCellDisappear = setTimeout(function () {
+                    cell.classList.remove('active');
+                    clearTimeout(makeCellDisappear);
+                }, timeActive);
+
+                // Add click event to make the cell winnable
+                cell.addEventListener("mousedown", makeWinnable);
+                TimeoutCellWinnable = setTimeout(function () {
+                    cell.removeEventListener("mousedown", makeWinnable);
+                    clearTimeout(TimeoutCellWinnable);
+                }, timeActive);
+
+            }
+            else {
+                alert('Une erreur est survenu, sorry.');
+                clearInterval(changeCell);
+            }
+
+        }
+
+    }
+
+    /**
+     * Function for XMLHttpRequest.onload method, check if the user win and stop the game
+     */
+    function checkIfWin() {
+
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+
+            if (xhr.status === 200) {
+
+                cell.removeEventListener("mousedown", makeWinnable);
+
+                start.innerText = "Play ?";
+
+                start.addEventListener("click", startGame, { once:true });
+
+                cell.classList.remove("active");
+                cell.classList.add("win");
+
+                alert(xhr.responseText);
+
+                clearInterval(changeCell);
+                clearTimeout(makeCellDisappear);
+                clearTimeout(TimeoutCellWinnable);
+            }
+            else {
+
+                alert('Une erreur est survenu, sorry.');
+
+                clearInterval(changeCell);
+                clearTimeout(makeCellDisappear);
+                clearTimeout(TimeoutCellWinnable);
+            }
+
+        }
+
+    }
 
 })();
