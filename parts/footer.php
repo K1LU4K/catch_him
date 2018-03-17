befa90016f0c619961dcb68181c0152ceea5eb73
Gautier <gautier.dujarrier@gmail.com>
2018-03-17 13:19:15 +0100

    Fusion of js files in app.js

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,6 @@
 
 </div>
 
-<script type="text/javascript" src="./assets/js/libs/functions.js"></script>
-<script type="text/javascript" src="./assets/js/navigation.js"></script>
 <script type="text/javascript" src="./assets/js/app.js"></script>
 
 </body>
